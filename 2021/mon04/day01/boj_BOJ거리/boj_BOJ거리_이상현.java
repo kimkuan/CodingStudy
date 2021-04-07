980c41bff41b7d5e348494e81fc90b67077f797c
kimkuan <zxd9857@gmail.com>
2021-04-07 23:35:42 +0900

    fix 2021

---
+++ 2021/mon04/day01/boj_BOJ거리/boj_BOJ거리_이상현.java
@@ -0,0 +1,52 @@
+package mon04.day01.boj_BOJ거리;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class boj_BOJ거리_이상현 {
+	
+	static int N;
+	static char[] arr;
+	static int[] dp;
+	static int INF = 987654321;
+	static int answer = INF;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		arr = br.readLine().toCharArray();
+		dp = new int[N];
+		Arrays.fill(dp, -1); // 아직 방문하지 않은 곳은 -1로 설정
+		
+		jump(0);
+		System.out.println(dp[N-1] == 0 ? dp[0] : -1);
+	}
+	
+	public static int jump(int cnt) { // return : dp값
+		if(cnt == N-1) { 
+			dp[cnt] = 0; // 방문 여부
+			return 0;
+		}
+		
+		if(dp[cnt] != -1) // 이미 방문한 곳 = 최솟값을 구한 곳 (중복 방지)
+			return dp[cnt]; 
+		dp[cnt] = INF; // -1이면 아직 방문하지 않은 곳이므로 min 계산을 위해 초기화 (단, MAX_VALUE는 오버플로우 발생가능함)
+		
+		char next = next(arr[cnt]);	
+		for(int i = cnt+1; i < N; i++) {
+			if(arr[i] == next) {
+				// 비용 = 현재 위치에서 다음 위치로 이동하는데 걸리는 비용 + 다음 위치에서 도착지까지의 비용
+				dp[cnt] = Math.min(dp[cnt], jump(i) + (i-cnt) * (i-cnt));
+			}
+		}
+		return dp[cnt];
+	}
+	
+	public static char next(char c) {
+		if(c == 'B') return 'O';
+		else if(c == 'O') return 'J';
+		else return 'B';
+	}
+}
---
+++ 2021/mon04/day05/boj_거짓말/boj_거짓말_이상현.java
@@ -0,0 +1,104 @@
+package mon04.day05.boj_거짓말;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class boj_거짓말_이상현 {
+	
+	static int N, M; // 사람의 수, 파티의 수
+	static int lieParty = 0; // 거짓말을 할 수 있는 파티의 수
+	static int[] parent;
+	static int[][] party;
+	static HashSet<Integer> peopleKnowTrue = new HashSet<>(); // 진실을 알고있는사람들
+	
+	public static void main(String[] args) throws IOException {
+		input();
+		solve();
+		System.out.println(lieParty);
+	}	
+	
+	private static void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken()); // 사람의 수
+		M = Integer.parseInt(st.nextToken()); // 파티의 수
+		parent = new int[N+1]; // 사람의 수
+		party = new int[M][]; // 파티의 수
+		
+		// parent를 자기자신으로 초기화
+		for(int i = 1; i <= N; i++) 
+			parent[i] = i;
+		
+		// 진실을 아는 사람의 수
+		st = new StringTokenizer(br.readLine());
+		int know = Integer.parseInt(st.nextToken());
+		for(int i = 0; i < know; i++) 
+			peopleKnowTrue.add(Integer.parseInt(st.nextToken())); 
+		
+		// 파티의 수만큼 for문
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int people = Integer.parseInt(st.nextToken()); // i번 파티에 참가하는 사람 수 (0~50)
+			party[i] = new int[people];
+			
+			for(int j = 0; j < people; j++) 
+				party[i][j] = Integer.parseInt(st.nextToken()); // 저장
+		}
+	}
+	
+	private static void solve() {
+		// 파티의 수만큼 for문
+		for(int i = 0; i < M; i++) {
+			int people = party[i].length; // 파티에 오는 사람의 수
+			
+			if(people == 0 || people == 1) // 사람의 수가 0명이거나 1명이면 넘어감
+				continue;
+			
+			int first = party[i][0]; // 첫번째 사람
+			for(int j = 1; j < people; j++) 
+				union(first, party[i][j]); // 하나의 집합으로 묶기
+		}
+		
+		// 거짓말할 수 잇는 파티인지 확인
+		for(int i = 0; i < M; i++) { 
+			boolean lie = true;
+			for(int j = 0; j < party[i].length; j++) {
+				int person = party[i][j];
+				if(peopleKnowTrue.contains(find(parent[person]))) // 진실을 알고있는 사람과 함께있는 기회가 있다면
+					lie = false; // 거짓말 할 수 없음
+			}
+			if(lie) lieParty++; // 진실을 모르는 사람들만 있는 파티에서는 거짓말을 할 수 있다
+		}
+	}
+	
+	private static int find(int x) {
+		if(x == parent[x]) return x;
+		return parent[x] = find(parent[x]);
+	} 
+	
+	private static void union(int a, int b) {
+		a = find(a);
+		b = find(b);
+		
+		// 진실을 알고 있는 사람
+		boolean knowA = peopleKnowTrue.contains(a);
+		boolean knowB = peopleKnowTrue.contains(b);
+
+		if(knowA)
+			parent[b] = a;
+		else if(knowB)
+			parent[a] = b;
+		else {// 둘 다 알고 있거나 둘 다 모르면
+			if(a < b) parent[b] = a; // 더 작은 값을 부모로 선택
+			else parent[a] = b;
+		}		
+	}
+}
+
+// Union find해서 같은 집합안에 있는지 확인
+// 단, 부모를 갱신할 때 우선수위가
+// 1. 진실을 알고 있는 사람
+// 2. 둘 다 진실을 알고 있다면 그 중 작은 숫자를 부모로 지정
