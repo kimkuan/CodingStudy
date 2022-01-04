e7c121b81e723378443f0236e12fb12e1d2557e4
kimkuan <zxd9857@gmail.com>
2022-01-05 00:02:06 +0900

    solve: boj_개똥벌레

--- 2022/mon01/day04/boj_개똥벌레/boj_개똥벌레.java
+++ 2022/mon01/day04/boj_개똥벌레/boj_개똥벌레.java
@@ -44,7 +44,7 @@ public class boj_개똥벌레 {
 	}
 	
 	private static int binarySearch(int left, int right, int[] arr, int h) {
-		int temp = Integer.MAX_VALUE; // 파괴한 개수
+		int idx = Integer.MAX_VALUE; // 처음 부딪히게 되는 물체의 인덱스
 		
 		while(left <= right) {
 			int mid = (left+right)/2;
@@ -53,18 +53,18 @@ public class boj_개똥벌레 {
 			// 왜냐면, 현재 높이에서 석순과 닿은 높이보다 크거나 같은 높이의 석순들은 당연히 파괴될 것이기 때문에
 			// 따라서 현재 높이와 닿는 석순의 최소 높이를 구하는 것!
 			if(arr[mid] >= h) {
-				temp = mid;
+				idx = mid;
 				right = mid - 1;
 			}
 			else
 				left = mid + 1;
 		}
-		return temp == Integer.MAX_VALUE ? 0 : size-temp;
+		return idx == Integer.MAX_VALUE ? 0 : size-idx;
 	}
 	
 	private static void findLeastHeight(int h) {
-		int downCount = binarySearch(0, size-1, down, h);
-		int upCount = binarySearch(0, size-1, up, H-h+1);
+		int downCount = binarySearch(0, size-1, down, h); // 석순의 경우에는 h=3일 때 h=3(h)보다 크거나 같은 경우 파괴됨
+		int upCount = binarySearch(0, size-1, up, H-h+1); // 종유석의 경우에는 h=3일 때는 h=5(H-h+1)보다 크거나 같은 경우 파괴됨
 				
 		int sum = downCount + upCount;
 		
